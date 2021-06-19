1eb74e1dcd666804f560649f28617fa415a5069a
JosueOb <josuericardocando@hotmail.com>
2021-06-19 18:54:13 -0500

    Tipos básicos y conceptos generales

---
+++ src/exercises/basic-types.ts
@@ -0,0 +1,18 @@
+/*
+    ===== Código de TypeScript =====
+*/
+let first_name: string = "Josué"; //declarar una variable
+const value = "2021"; //valor constante
+
+first_name = "Ricardo";
+//first_name = 123;
+
+let hp: number | string = 95;
+hp = 'Full';
+
+let safe: boolean = true;
+//safe = 0;
+
+
+
+console.log(first_name, hp, safe);
